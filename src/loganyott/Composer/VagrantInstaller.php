1b63d3e75dd364041a0e32883c6a5906f3f937ab
Logan Yott <loganyott@gmail.com>
2015-06-30 15:46:14 -0600

    Fixing path in Vagrantfile generation

--- src/loganyott/Composer/VagrantInstaller.php
+++ src/loganyott/Composer/VagrantInstaller.php
@@ -17,13 +17,13 @@ class VagrantInstaller extends LibraryInstaller {
     if(!file_exists($vagrantfile)) {
       throw new \RuntimeException("\nCouldn't find package's Vagrantfile.");
     }
-    if(!($cwd = getcwd())) {
+    if( !( $cwd = getcwd() ) ) {
       throw new \RuntimeException("\nCan't access cwd.");
     }
 
-    $vagrantfile = dirname(dirname(dirname(dirname(__FILE__)))) . '/Vagrantfile';
-    $template = file_get_contents( $vagrantfile );
-    $template = str_replace( "# VAGRANTFILE = ", "VAGRANTFILE = $vagrantfile", $template );
+    $template = dirname(dirname(dirname(dirname(__FILE__)))) . '/Vagrantfile';
+    $template = file_get_contents( $template );
+    $template = str_replace( "# VAGRANTFILE = ", "VAGRANTFILE = \"$vagrantfile\"", $template );
 
     if ( false === file_put_contents( $cwd . "/Vagrantfile", $template ) ) {
       throw new \RuntimeException( "\nCouldn't write to Vagrantfile in project root." );
